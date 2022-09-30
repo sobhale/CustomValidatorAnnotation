c0eb2e27ebd5f6fa7a0c893003321f73ef0e4622
Bhale, Someshwar <sobhale@deloitte.com>
2022-09-30 16:37:49 +0530

    Added loggers using log4j

--- src/main/java/customvalidator/service/UserService.java
+++ src/main/java/customvalidator/service/UserService.java
@@ -2,6 +2,7 @@ package customvalidator.service;
 
 import customvalidator.model.User;
 import customvalidator.repository.UserRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,27 +15,39 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger LOGGER = Logger.getLogger(UserService.class);
+
+    private static final String USER_NOT_FOUND_MSG = "User not found with user Id: ";
+
     public List<User> fetchAllUsers(){
+        LOGGER.info("Fetching all users from DB");
         return userRepository.findAll();
     }
 
     public User saveUser(User user){
+        LOGGER.info("Saving a user to DB with user name: " + user.getName());
+        LOGGER.debug("User being saved is : " + user);
         return userRepository.save(user);
     }
 
     public User fetchUserById(Long userId){
+        LOGGER.info("Fetching a user with Id: " + userId);
         return userRepository.findById(userId).orElseThrow(
                 () -> {
-                    throw new ResourceNotFoundException("User not found with User Id: " + userId);
+
+                    LOGGER.error(USER_NOT_FOUND_MSG + userId);
+                    throw new ResourceNotFoundException(USER_NOT_FOUND_MSG + userId);
                 }
         );
     }
 
     public User saveOrUpdateUser(User user, Long userId) {
 
+        LOGGER.info("Updating user with Id: " + userId);
         User foundUser = userRepository.findById(userId)
                 .orElseThrow( () -> {
-                    throw new ResourceNotFoundException("User not found with User Id: " + userId);
+                    LOGGER.error(USER_NOT_FOUND_MSG + userId);
+                    throw new ResourceNotFoundException(USER_NOT_FOUND_MSG + userId);
                 });
                 foundUser.setAddress(user.getAddress());
                 foundUser.setEmail(user.getEmail());
@@ -47,9 +60,11 @@ public class UserService {
 
     public String deleteUser(Long userId) {
 
+        LOGGER.info("Deleting a user with Id: " + userId);
         User user = userRepository.findById(userId).orElseThrow(
                 () -> {
-                    throw new ResourceNotFoundException("User not found with User Id: " + userId);
+                    LOGGER.error(USER_NOT_FOUND_MSG + userId);
+                    throw new ResourceNotFoundException(USER_NOT_FOUND_MSG + userId);
                 });
 
        userRepository.delete(user);
